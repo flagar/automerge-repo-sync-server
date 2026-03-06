4b9f53caf8f0bf845d68e76a0b3460548b214d10
Flavio Gargiulo <flagar@gmail.com>
2026-03-06 13:02:43 +0100

    Manual and automatic locks

--- src/locks.js
+++ src/locks.js
@@ -89,7 +89,7 @@ export function removeEditionSectionLocks(msg) {
         }
         let initial_sections_length = locks_data.sections.length;
         locks_data.sections = locks_data.sections.filter(lock => {
-            return !(lock.user.id == msg.data.user.id && lock.edition_id == msg.data.edition_id);
+            return !(lock.user.id == msg.data.user.id && lock.edition_id == msg.data.edition_id && lock.manual === false);
         });
         if (initial_sections_length != locks_data.sections.length) {
             actually_remove = true;
